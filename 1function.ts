77bc2106ad3fb30bbd08026f6a760a2590d51e66
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-15 21:06:33 +0500

    Update 1function.ts

--- 1function.ts
+++ 1function.ts
@@ -18,8 +18,24 @@ let addnum: (x: number, y: number) => number = function (a, b) {
 };
 
 //3. callback function
+
+// method1
 type fn2 = (a: string) => void;
 
 function fun1(callback: fn2) {
   callback("hello");
 }
+
+// method2
+function myCallBack(text: string) {
+  console.log("inside myCallback " + text);
+}
+
+function callingFunction(
+  initialText: string,
+  callback: (text: string) => void
+) {
+  callback(initialText);
+}
+
+callingFunction("myText", myCallBack);

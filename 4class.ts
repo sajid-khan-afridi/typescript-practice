a9b3d6c907d2db77f6110d9dc3101f0e2da72b4d
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-18 08:01:07 +0500

    class

--- 4class.ts
+++ 4class.ts
@@ -1,30 +1,67 @@
 // 1. Defining classes
 
-//1.1 Class declarations
-class Rectangle {
-    constructor(height, width) {
-      this.height = height;
-      this.width = width;
-    }
-  }
+// //1.1 Class declarations
+// class Rectangle {
+//   height: string;
+//   width: string;
+//   constructor(height: string, width: string) {
+//     this.height = height;
+//     this.width = width;
+//   }
+// }
 
-  //1.2 Class expressions
-  
-  //unnamed
-  let Rectangle2 = class {
-    constructor(height, width) {
-      this.height = height;
-      this.width = width;
-    }
-  };
-  console.log(Rectangle.name); // "Rectangle"
+// //1.2 Class expressions
 
-  // named
+// //unnamed
+// let Rectangle2 = class {
+//   height: string;
+//   width: string;
+//   constructor(height: string, width: string) {
+//     this.height = height;
+//     this.width = width;
+//   }
+// };
+// console.log(Rectangle.name); // "Rectangle"
+
+// // named
 // let Rectangle3 = class Rectangle4 {
-//   constructor(height, width) {
+//   height: string;
+//   width: string;
+//   constructor(height: string, width: string) {
 //     this.height = height;
 //     this.width = width;
 //   }
 // };
 // console.log(Rectangle.name); // "Rectangle2"
-// console.log(Rectangle4.name); // error
+// //console.log(Rectangle4.name); // error
+
+// //Miss Faiza Aziz class code practice
+class Car {
+  model: string;
+  color: string;
+  licence!: string;
+  constructor(model: string, color: string) {
+    this.model = model;
+    this.color = color;
+  }
+  displayAll() {
+    return `${this.color} ${this.model}`;
+  }
+  get Licence() {
+    return this.licence;
+  }
+  set Licence(val: string) {
+    this.licence = val;
+  }
+}
+const civic = new Car("2022", "blue");
+const honda = new Car("2000", "black");
+
+console.log(civic.displayAll());
+console.log(honda.displayAll());
+
+honda.Licence = "str123"; //set is used here
+console.log(honda.Licence); //get is used here
+
+honda.licence = "newstr";
+console.log(honda.licence);

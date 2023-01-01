0433d6b94c83a4c09bf33fc11bc7cb5f9cfdfc72
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-01 12:54:27 +0500

    c

--- 7class.js
+++ 7class.js
@@ -1,14 +1,42 @@
-//                      //readonly
-var Greeter = /** @class */ (function () {
-    function Greeter(name) {
+//                      //class
+var Student = /** @class */ (function () {
+    function Student(name) {
         this.name = name;
     }
-    Greeter.prototype.err = function () {
-        // this.name = "not ok";
-        //Cannot assign to 'name' because it is a read-only property.
+    Student.prototype.getName = function () {
+        return this.name;
     };
-    return Greeter;
+    return Student;
 }());
-var obj = new Greeter("another value");
-console.log(obj.name);
+var obj = new Student("Ahmad");
+console.log(obj.getName());
+//                      //readonly
+// class Greeter {
+//   readonly name: string;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+//   err() {
+//     this.name = "not ok";
+//     // Cannot assign to 'name' because it is a read-only property.
+//   }
+// }
+// const obj = new Greeter("another value");
+// console.log(obj.name);
 // g.name = "also not ok";
+//                            //Super Calls
+// class Base {
+//   k = 4;
+//   constructor(kValue: number) {
+//     this.k = kValue;
+//   }
+// }
+// class Derived extends Base {
+//   constructor() {
+//     // Prints a wrong value in ES5; throws exception in ES6
+//     super(5);
+//     console.log(this.k);
+//     // 'super' must be called before accessing 'this' in the constructor of a derived class.
+//   }
+// }
+// export {};
--- 7class.ts
+++ 7class.ts
@@ -1,17 +1,48 @@
-//                      //readonly
+//                      //class
 
-class Greeter {
-  readonly name: string;
-
+class Student {
+  name: string;
   constructor(name: string) {
     this.name = name;
   }
-
-  err() {
-    // this.name = "not ok";
-    //Cannot assign to 'name' because it is a read-only property.
+  getName() {
+    return this.name;
   }
 }
-const obj = new Greeter("another value");
-console.log(obj.name);
+let obj = new Student("Ahmad");
+console.log(obj.getName());
+
+//                      //readonly
+
+// class Greeter {
+//   readonly name: string;
+
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   err() {
+//     this.name = "not ok";
+//     // Cannot assign to 'name' because it is a read-only property.
+//   }
+// }
+// const obj = new Greeter("another value");
+// console.log(obj.name);
 // g.name = "also not ok";
+
+//                            //Super Calls
+// class Base {
+//   k = 4;
+//   constructor(kValue: number) {
+//     this.k = kValue;
+//   }
+// }
+
+// class Derived extends Base {
+//   constructor() {
+//     // Prints a wrong value in ES5; throws exception in ES6
+//     super(5);
+//     console.log(this.k);
+//     // 'super' must be called before accessing 'this' in the constructor of a derived class.
+//   }
+// }
